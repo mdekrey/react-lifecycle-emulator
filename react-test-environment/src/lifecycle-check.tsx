9ce79e515c8317afa72fc52a1f3907821cdaf389
Matt DeKrey <mattdekrey@gmail.com>
2017-04-15 23:33:45 -0500

    Refactor to hope to reuse

--- react-test-environment/src/lifecycle-check.tsx
+++ react-test-environment/src/lifecycle-check.tsx
@@ -1,20 +1,26 @@
 import * as React from 'react';
 
-export interface IExternalProps {
+export interface IRecordedProps {
   counterExternal: number;
+}
+export interface IControlsProps {
   updateCounter(newValue: number, finished: () => void): void;
+  logNormal(caller: keyof React.ComponentLifecycle<any, any>, props: IPropsState<IRecordedProps, IInternalProps>): void;
+  logChanging(caller: keyof React.ComponentLifecycle<any, any>, props: IChangingProps<IRecordedProps, IInternalProps>): void;
 }
-interface IInternalProps { counterInternal: number; }
-interface IChangingProps<P, S> {
-  nextProps?: P;
-  nextState?: S;
-
-  prevProps?: P;
-  prevState?: S;
+export interface IInternalProps { counterInternal: number; }
+export interface IPropsState<P, S> {
+  props: P;
+  state: S;
+}
+export interface IChangingProps<P, S> {
+  current: IPropsState<P, S>;
+  next: IPropsState<P, S>;
+  prev: IPropsState<P, S>;
 }
 
-class LifecycleCheckTarget extends React.Component<IExternalProps, IInternalProps> {
-  constructor(props: IExternalProps) {
+class LifecycleCheckTarget extends React.Component<IRecordedProps & IControlsProps, IInternalProps> {
+  constructor(props: IRecordedProps & IControlsProps) {
     super(props);
     this.state = {
       counterInternal: 0,
@@ -27,22 +33,22 @@ class LifecycleCheckTarget extends React.Component<IExternalProps, IInternalProp
   componentDidMount() {
     this.logNormal('componentDidMount');
   }
-  componentWillReceiveProps(nextProps: Readonly<IExternalProps>) {
-    this.logChanging('componentWillReceiveProps', { nextProps });
+  componentWillReceiveProps(nextProps: Readonly<IRecordedProps & IControlsProps>) {
+    this.logChanging('componentWillReceiveProps', { next: { props: this.clean(nextProps), state: this.state } });
 
     if (nextProps.counterExternal === 4) {
       this.setState(() => ({ counterInternal: 4 }), () => console.log('setState callback'));
     }
   }
-  shouldComponentUpdate(nextProps: Readonly<IExternalProps>, nextState: Readonly<IInternalProps>): boolean {
-    this.logChanging('shouldComponentUpdate', { nextProps, nextState });
+  shouldComponentUpdate(nextProps: Readonly<IRecordedProps & IControlsProps>, nextState: Readonly<IInternalProps>): boolean {
+    this.logChanging('shouldComponentUpdate', { next: { props: this.clean(nextProps), state: nextState } });
     return true;
   }
-  componentWillUpdate(nextProps: Readonly<IExternalProps>, nextState: Readonly<IInternalProps>): void {
-    this.logChanging('componentWillUpdate', { nextProps, nextState });
+  componentWillUpdate(nextProps: Readonly<IRecordedProps & IControlsProps>, nextState: Readonly<IInternalProps>): void {
+    this.logChanging('componentWillUpdate', { next: { props: this.clean(nextProps), state: nextState } });
   }
-  componentDidUpdate(prevProps: Readonly<IExternalProps>, prevState: Readonly<IInternalProps>): void {
-    this.logChanging('componentDidUpdate', { prevProps, prevState });
+  componentDidUpdate(prevProps: Readonly<IRecordedProps & IControlsProps>, prevState: Readonly<IInternalProps>): void {
+    this.logChanging('componentDidUpdate', { prev: { props: this.clean(prevProps), state: prevState } });
 
     if (this.state.counterInternal === 0) {
       setTimeout(() => {
@@ -81,45 +87,43 @@ class LifecycleCheckTarget extends React.Component<IExternalProps, IInternalProp
     this.logNormal('componentWillUnmount');
   }
 
-  private logNormal(caller: keyof React.ComponentLifecycle<any, any>) {
-    console.log({
-      caller,
-      current: Object.assign({}, this.props, this.state)
-    });
-  }
-
-  private logChanging(caller: keyof React.ComponentLifecycle<any, any>, props: IChangingProps<IExternalProps, IInternalProps>) {
-    const {
-      nextProps = this.props,
-      nextState = this.state,
-
-      prevProps = this.props,
-      prevState = this.state,
-    } = props;
-    console.log({
-      caller,
-      current: Object.assign({}, this.props, this.state),
-      prev: Object.assign({}, prevProps, prevState),
-      next: Object.assign({}, nextProps, nextState),
-    });
-  }
-
   render(): JSX.Element | null {
     return null;
   }
+
+  private clean(props: IRecordedProps & IControlsProps): IRecordedProps {
+    return {
+      counterExternal: props.counterExternal,
+    };
+  }
+
+  private logNormal(caller: keyof React.ComponentLifecycle<any, any>) {
+    this.props.logNormal(caller, { props: this.clean(this.props), state: this.state });
+  }
+
+  private logChanging(caller: keyof React.ComponentLifecycle<any, any>, props: Partial<IChangingProps<IRecordedProps, IInternalProps>>) {
+    const current = { props: this.clean(this.props), state: this.state };
+    const {
+      next = current,
+      prev = current,
+    } = props;
+    this.props.logChanging(caller, {
+      current,
+      next,
+      prev,
+    });
+  }
 }
 
-export class LifecycleCheck extends React.Component<{}, IExternalProps> {
+export class LifecycleCheck extends React.Component<{}, IRecordedProps> {
 
   constructor(props: {}) {
     super(props);
     this.state = {
       counterExternal: 0,
-      updateCounter: (newValue, callback) => this.setState({ counterExternal: newValue }, callback),
     };
 
     console.group('begin mount');
-
   }
 
   componentDidMount() {
@@ -132,6 +136,31 @@ export class LifecycleCheck extends React.Component<{}, IExternalProps> {
   }
 
   render() {
-    return <LifecycleCheckTarget {...this.state} />;
+    return (
+      <LifecycleCheckTarget
+        {...this.state}
+        updateCounter={this.updateCounter}
+        logNormal={this.logNormal}
+        logChanging={this.logChanging} />
+    );
   }
+
+  private updateCounter = (newValue: number, callback: () => void) => this.setState({ counterExternal: newValue }, callback);
+
+  private logNormal(caller: keyof React.ComponentLifecycle<any, any>, {props, state}: IPropsState<IRecordedProps, IInternalProps>) {
+    console.log({
+      caller,
+      current: Object.assign({}, props, state),
+    });
+  }
+
+  private logChanging(caller: keyof React.ComponentLifecycle<any, any>, p: IChangingProps<IRecordedProps, IInternalProps>) {
+    console.log({
+      caller,
+      current: Object.assign({}, p.current.props, p.current.state),
+      prev: Object.assign({}, p.prev.props, p.prev.state),
+      next: Object.assign({}, p.next.props, p.next.state),
+    });
+  }
+
 }
